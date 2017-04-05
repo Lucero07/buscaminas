b5c72f85f8cd0fe6f618c83000c6dd681729a428
Lucero07 <ing.lucero.ramos@gmail.com>
2017-04-05 12:28:15 -0500

    BuscaminasDinamicoconUsuario

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 // variables de configuración
 var segTranscurridos = 0;
-var ancho = 5;
-var alto = 10;
+var ancho = parseInt(prompt("¿cuantas colunmas quieres?"));
+var alto = parseInt(prompt("¿cuantas filas quieres?"));
 var numMinas = parseInt(prompt("¿cuantas minas quieres?"));
 var numIntentos = (ancho * alto) - numMinas;
 var iniciar = document.getElementById('iniciar');
 iniciar.onclick = iniciarJuego;
-var imagen = document.createElement("img");
-imagen.src = "assets/bomba.jpg";
+//var imagen = document.createElement("img");
+//imagen.src = "assets/bomba.jpg";
 
 
 function iniciarJuego() {
@@ -49,15 +49,15 @@ function posicionactual() {
     if (!numIntentos) {
         alert('Felicidades Ganaste!');
         location.href = 'index.html';
-    }
+    };
 };
 
 //hacer dinamico con el random
-function crearCampoJuego(ancho, alto, minaTotal) {
+function crearCampoJuego(ancho, alto, minasTotales) {
     var campoActual = hacerTablaJuego(ancho, alto);
     var contadorMinas = 0;
 
-    while (contadorMinas < minaTotal) {
+    while (contadorMinas < minasTotales) {
         var minaDinamica = aleatorio(0, 1);
         var posicionDinamica = [aleatorio(0, ancho - 1), aleatorio(0, alto - 1)];
 
@@ -66,11 +66,11 @@ function crearCampoJuego(ancho, alto, minaTotal) {
             if (minaDinamica) {
                 campoActual[posicionDinamica[0]][posicionDinamica[1]] = 'BOOM!!';
 
-                for (var x = (posicionDinamica[0] - 1); x <= (posicionDinamica[0] + 1); x++) {
-                    for (var y = (posicionDinamica[1] - 1); y <= (posicionDinamica[1] + 1); y++) {
+                for (var posicionX = (posicionDinamica[0] - 1); posicionX <= (posicionDinamica[0] + 1); posicionX++) {
+                    for (var posicionY = (posicionDinamica[1] - 1); posicionY <= (posicionDinamica[1] + 1); posicionY++) {
                         //Busque porque al generar todo aleatorio para algunos numeros me creaba un error y encontre que para poder realizar acciones debemos decirle a la computadora vuelve a ejecutar el evento
                         try { //Las sentencias que serán ejecutadas
-                            campoActual[x][y] += (campoActual[x][y] != 'BOOM!!') ? 1 : '';
+                            campoActual[posicionX][posicionY] += (campoActual[posicionX][posicionY] != 'BOOM!!') ? 1 : '';
                         } catch (e) {} //Sentencias que son ejecutadas si una excepción es lanzada en el bloque try.
                     };
                 };
@@ -96,8 +96,8 @@ function hacerTablaJuego(ancho, alto) {
     return campoJuego;
 };
 
-function dibujarMinas(ancho, alto, elementId) {
-    var div = document.getElementById(elementId);
+function dibujarMinas(ancho, alto, elemnto) {
+    var div = document.getElementById(elemnto);
     div.innerHTML = '';
 
     for (var i = 0; i < alto; i++) {
@@ -107,18 +107,3 @@ function dibujarMinas(ancho, alto, elementId) {
         div.innerHTML += '<br/>';
     };
 };
-// for (var i = 0; i < alto; i++) {
-//     for (var j = 0; j < ancho; j++) {
-//
-//         var comodin = document.createElement('div');
-//         var cajaIndividual = document.createElement("button");
-//
-//         cajaIndividual.type = "button";
-//         cajaIndividual.name = "botonsitos"
-//         cajaIndividual.id = "' + i + '-' + j + '";
-//         cajaIndividual.style = "width:30px; height:30px"
-//       div.appendChild(cajaIndividual);
-//     };
-//     div.innerHTML += '<br/>';
-// };
-// };
